5f3517a79f9baf74c4f673496a9916bfc7de81d0
Fella_Dolphin <zair@capusta.space>
2023-03-19 16:39:11 +0300

    init commit

---
+++ src/components/ElementList.tsx
@@ -0,0 +1,9 @@
+import style from '../styles/module/ElementListStyles.module.scss'
+
+const ElementList = () => {
+    return (
+        <div className={style.title}>Hello MIR!</div>
+    )
+}
+
+export default ElementList;
